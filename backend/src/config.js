38c90206ba901af2c0e15986bafb9fc0b20a6bea
hatters madness <daniel.maclagan@gmail.com>
2022-04-22 08:59:08 -0400

    updated presale start date

--- backend/src/config.js
+++ backend/src/config.js
@@ -60,10 +60,10 @@ const MINT_PRICE = 1; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. 
 const TOKENS_PER_MINT = 0.001; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!  // make this 8-10 when moving to polygon
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-04-22T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-05-22T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-05-02T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x0A14Cb37ead5ae506D86F3eaa9B96158Fc7233Cc"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
