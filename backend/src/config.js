bc5e28cbce9443aee9b45c51f832c473cb021137
hatters madness <daniel.maclagan@gmail.com>
2022-04-22 09:19:02 -0400

    update tokens_mint

--- backend/src/config.js
+++ backend/src/config.js
@@ -56,8 +56,8 @@ const METADATA_UPDATABLE = true; // set to false if you don't want to allow meta
 const OWNER_ADDRESS = '0x0A14Cb37ead5ae506D86F3eaa9B96158Fc7233Cc';
 const TREASURY_ADDRESS = '0x0A14Cb37ead5ae506D86F3eaa9B96158Fc7233Cc';
 const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 0.001; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
-const TOKENS_PER_MINT = 1; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!  // make this 8-10 when moving to polygon
+const MINT_PRICE = 0.001; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED! // make this 8-10 when moving to polygon
+const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!  
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PUBLIC_MINT_START_DATE = "2022-05-12T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
