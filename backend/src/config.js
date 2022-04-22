919689b16c392617443de5022776d52c5abc936a
hatters madness <daniel.maclagan@gmail.com>
2022-04-22 09:04:56 -0400

    updated presale whitelist

--- backend/src/config.js
+++ backend/src/config.js
@@ -68,7 +68,7 @@ const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the ro
 const ROYALTY_ADDRESS = "0x0A14Cb37ead5ae506D86F3eaa9B96158Fc7233Cc"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = ["0x0A14Cb37ead5ae506D86F3eaa9B96158Fc7233Cc"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
